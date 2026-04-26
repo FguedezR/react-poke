7303b3719698057f0e1e4bca9ed2fd6d62108c5b
Fernando <fernanguedezr@gmail.com>
2026-04-26 20:21:59 +0200

    fix: update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // guarda lo que el usuario escribe en el input
+  const [searchTerm, setSearchTerm] = useState("");
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+  //guarda los datos del pokemon cuando la api responde con exito
+  // null es todavia no hemos encontrado ninguno
+  const [pokemon, setPokemon] = useState(null);
 
-      <div className="ticks"></div>
+  //true mientras se espera la respuesta de la api
+  const [loading, setLoading] = useState(false);
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+  //guarda el mensaje de error si falla api o pokemon no encontrado
+  const [error, setError] = useState(null);
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+  useEffect(() => {
+    //campo está vacío, reseteamos todo y salimos.
+    //evitamos llamadas innecesarias a la API.
+    if (searchTerm.trim() === "") {
+      setPokemon(null);
+      setError(null);
+      return;
+    }
+
+    const timer = setTimeout(() => {
+      fetchPokemon(searchTerm.trim().toLowerCase());
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [searchTerm]);
 }
 
-export default App
+const fetchPokemon = async (name) => {
+  setLoading(true);
+  setError(null);
+  setPokemon(null);
+
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    if (!response.ok) {
+      if (response.status === 404) {
+        throw new Error(`No se encontró ningún Pokémon llamando ${name}`);
+      }
+      throw new Error(`Error en la API: ${response.status}`);
+    }
+    const data = await response.json();
+
+    setPokemon({
+      id: data.id,
+      name: data.name,
+      image: data.sprites,
+    })
+  } catch (err) {
+  } finally {
+  }
+};
+
+export default App;
